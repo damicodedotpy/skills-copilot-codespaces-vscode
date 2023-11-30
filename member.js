5250ab8bcdac7a6e1fdb127adbf1c8024e75ef70
Damian Lazos <147671562+damicodedotpy@users.noreply.github.com>
2023-11-30 23:24:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember( {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberPath = document.getElementById("memberPath");
+    var skillsPath = document.getElementById("skillsPath");
+    var projectsPath = document.getElementById("projectsPath");
+    var contactPath = document.getElementById("contactPath");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    memberPath.style.color = "#fff";
+    skillsPath.style.color = "#000";
+    projectsPath.style.color = "#000";
+    contactPath.style.color = "#000";
